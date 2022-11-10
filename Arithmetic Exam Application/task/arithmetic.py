8425eb8c58bb68b1749d397c849aab8fc10ad62b
denys <denamyte@gmail.com>
2022-11-10 13:35:22 +0200

    2022-11-10: Stage 1/4

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,1 +1,15 @@
-# write your code here
+def main():
+    res = 0
+    a, sign, b = input().split()
+    a, b = int(a), int(b)
+    match sign:
+        case '+':
+            res = a + b
+        case '-':
+            res = a - b
+        case '*':
+            res = a * b
+    print(res)
+
+
+main()
