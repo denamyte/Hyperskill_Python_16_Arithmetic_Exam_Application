03a9323e39db9924e82027c0c9ed78490f7d6e6e
denys <denamyte@gmail.com>
2022-11-10 14:05:46 +0200

    2022-11-10: Stage 2/4

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,15 +1,27 @@
+from random import randint, choice
+from typing import Dict, Callable
+
+SIGNS = ['+', '-', '*']
+OPS: Dict[str, Callable[[int, int], int]] = {
+    '+': lambda a, b: a + b,
+    '-': lambda a, b: a - b,
+    '*': lambda a, b: a * b,
+}
+
+
+def random_int() -> int:
+    return randint(2, 9)
+
+
+def solve(task: str):
+    a, sign, b = task.split()
+    return OPS[sign](int(a), int(b))
+
+
 def main():
-    res = 0
-    a, sign, b = input().split()
-    a, b = int(a), int(b)
-    match sign:
-        case '+':
-            res = a + b
-        case '-':
-            res = a - b
-        case '*':
-            res = a * b
-    print(res)
+    task = ' '.join(str(x) for x in (random_int(), choice(SIGNS), random_int()))
+    print(task)
+    print('Right!' if str(solve(task)) == input() else 'Wrong!')
 
 
 main()
